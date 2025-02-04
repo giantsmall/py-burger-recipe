29a33cec6b2d30c88de62aa0b9304d621ca9ffbb
Marek Smolana <msmolana@gmail.com>
2025-02-04 12:58:59 +0100

    darft v2

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 from __future__ import annotations
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Validator(ABC):
     def __set_name__(self, owner: any, name: str) -> None:
         self.protected_name = "_" + name
-        print(f"validator: name = {self.protected_name}")
 
-    def __get__(self) -> int:
+    def __get__(self, value: any, value2: any) -> int:
         return self._value
 
     def __set__(self, value: int, third: any = 0) -> None:
-        self.validate(value)
-        self._value = value
+        try:
+            self.validate(value)
+            self._value = value
+        except TypeError as error:
+            print(error)
+        except ValueError as error:
+            print(error)
 
     value = property(__get__, __set__)
 
+    @abstractmethod
     def validate(self) -> None:
         pass
 
@@ -26,13 +31,13 @@ class Number(Validator):
         self.max_value = max_value
         self._value = 5
 
-    def validate(self, value: any) -> None:
+    def validate(self, value: int) -> None:
         if not isinstance(value, int):
-            raise TypeError("Quantity should be integer")
+            raise TypeError(f"Quantity should be integer: {type(value)}")
         elif self.min_value > self._value or self.max_value < self._value:
             raise ValueError(f"Quantity should not be less than \
-                             {self.min_value} \
-                             and greater than {self.max_value}")
+                             attribute minvalue \
+                             and greater than attribute maxvalue.")
 
 
 class OneOf(Validator):
@@ -40,7 +45,7 @@ class OneOf(Validator):
         self.options = options
 
     def validate(self, value: str) -> None:
-        if value not in self.options:
+        if not value in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}")
 
 
@@ -52,6 +57,7 @@ class BurgerRecipe:
                  cutlest: int,
                  eggs: int,
                  sauce: str) -> None:
+        print(type(buns))
         self.buns = buns
         self.cheese = cheese
         self.tomatoes = tomatoes
@@ -65,3 +71,5 @@ class BurgerRecipe:
     cutlets = Number(1, 3)
     eggs = Number(0, 2)
     sauce = OneOf(["ketchup", "mayo", "burger"])
+
+burger = BurgerRecipe(1, 2, 2, 2, 2, "ketchup")
